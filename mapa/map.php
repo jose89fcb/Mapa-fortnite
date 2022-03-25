cc2bfd5603dec724a570b43a96435398edbc36de
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-03-25 03:13:29 +0100

    Add files via upload

---
+++ mapa/map.php
@@ -0,0 +1,40 @@
+<?php
+ 
+ 
+$lang= $_GET["lang"];
+ 
+error_reporting(0);
+ 
+$context = stream_context_create($opts);
+ 
+ 
+$file = file_get_contents("https://fortnite-api.com/v1/map?language=$lang", false, $context);
+ 
+ 
+ 
+ 
+ 
+$image_data = json_decode($file, true);
+$twitch = json_decode($file, true);
+ 
+ 
+$news = json_decode($news, true);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+?><?php
+error_reporting(0);
+//Get the file
+$content = file_get_contents($twitch['data']['images']['pois']);
+//Store in the filesystem.
+$fp = fopen("imagenes/map_$lang.png", "w");
+fwrite($fp, $content);
+fclose($fp);
+?>
+{
+"Mapa":"https://<?php echo $_SERVER['SERVER_NAME'];?>/map/imagenes/map_<?php echo $lang?>.png"
+}
